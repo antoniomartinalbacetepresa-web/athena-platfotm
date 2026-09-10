d7d09ecfc0947637516a5ec9cc8c1ed3dec07be5
antoniomartinalbacetepresa-web <antoniomartinalbacetepresa@gmail.com>
2026-09-10 12:18:51 +0200

    Add registration route

--- app/lib/core/routing/app_routes.dart
+++ app/lib/core/routing/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
   static const welcome = '/';
   static const login = '/login';
+  static const register = '/register';
   static const dashboard = '/dashboard';
   static const market = '/market';
   static const news = '/news';
