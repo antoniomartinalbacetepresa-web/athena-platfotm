4a52e992840206129f97653ed04f30153c810166
antoniomartinalbacetepresa-web <antoniomartinalbacetepresa@gmail.com>
2026-09-10 12:14:46 +0200

    Add login route

--- app/lib/core/routing/app_routes.dart
+++ app/lib/core/routing/app_routes.dart
@@ -1,5 +1,6 @@
 class AppRoutes {
   static const welcome = '/';
+  static const login = '/login';
   static const dashboard = '/dashboard';
   static const market = '/market';
   static const news = '/news';
