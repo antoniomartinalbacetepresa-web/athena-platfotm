a48fe6080f6a222ab1bca2fdfb3c6876b2a2db69
antoniomartinalbacetepresa-web <antoniomartinalbacetepresa@gmail.com>
2026-09-17 10:03:49 +0200

    Wire canonical ATHENA synthesis into Flutter dependencies

--- app/lib/features/recommendations/di/recommendation_dependencies.dart
+++ app/lib/features/recommendations/di/recommendation_dependencies.dart
@@ -1,8 +1,10 @@
+import '../controllers/athena_synthesis_controller.dart';
 import '../controllers/recommendation_learning_controller.dart';
 import '../data/datasources/athena_backend_professional_dossier_data_source.dart';
 import '../data/datasources/athena_backend_recommendation_learning_data_source.dart';
 import '../data/datasources/athena_backend_recommendation_production_data_source.dart';
 import '../data/datasources/athena_backend_recommendation_shadow_candidate_data_source.dart';
+import '../data/datasources/athena_backend_synthesis_data_source.dart';
 
 class RecommendationDependencies {
   static const String _defaultBackendUrl = String.fromEnvironment(
@@ -14,14 +16,18 @@ class RecommendationDependencies {
   final AthenaBackendRecommendationShadowCandidateDataSource shadowCandidateDataSource;
   final AthenaBackendRecommendationProductionDataSource productionDataSource;
   final AthenaBackendProfessionalDossierDataSource professionalDossierDataSource;
+  final AthenaBackendSynthesisDataSource synthesisDataSource;
   final RecommendationLearningController learningController;
+  final AthenaSynthesisController synthesisController;
 
   RecommendationDependencies({
     required this.learningDataSource,
     required this.shadowCandidateDataSource,
     required this.productionDataSource,
     required this.professionalDossierDataSource,
+    required this.synthesisDataSource,
     required this.learningController,
+    required this.synthesisController,
   });
 
   factory RecommendationDependencies.create({
@@ -45,20 +51,29 @@ class RecommendationDependencies {
     final professionalDossierDataSource = AthenaBackendProfessionalDossierDataSource(
       baseUrl: effectiveBaseUrl,
     );
+    final synthesisDataSource = AthenaBackendSynthesisDataSource(
+      baseUrl: effectiveBaseUrl,
+    );
 
     return RecommendationDependencies(
       learningDataSource: learningDataSource,
       shadowCandidateDataSource: shadowCandidateDataSource,
       productionDataSource: productionDataSource,
       professionalDossierDataSource: professionalDossierDataSource,
+      synthesisDataSource: synthesisDataSource,
       learningController: RecommendationLearningController(
         provider: learningDataSource,
+      ),
+      synthesisController: AthenaSynthesisController(
+        dataSource: synthesisDataSource,
       ),
     );
   }
 
   void dispose() {
+    synthesisController.dispose();
     learningController.dispose();
+    synthesisDataSource.dispose();
     learningDataSource.dispose();
     shadowCandidateDataSource.dispose();
     productionDataSource.dispose();
