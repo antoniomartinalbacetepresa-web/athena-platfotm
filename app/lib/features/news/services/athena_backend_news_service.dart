869418405a9bccd166c6054530c46426b3c158c1
antoniomartinalbacetepresa-web <antoniomartinalbacetepresa@gmail.com>
2026-09-19 12:04:01 +0200

    fix: satisfy Dart null-aware collection lint in news service

--- app/lib/features/news/services/athena_backend_news_service.dart
+++ app/lib/features/news/services/athena_backend_news_service.dart
@@ -42,7 +42,7 @@ class AthenaBackendNewsService {
       'limit': '$limit',
       'language': normalizedLanguage,
       'country': normalizedCountry,
-      if (normalizedQuery case final value?) 'query': value,
+      'query': ?normalizedQuery,
     };
     final uri = Uri.parse('$baseUrl/api/v1/news/feed').replace(
       queryParameters: params,
