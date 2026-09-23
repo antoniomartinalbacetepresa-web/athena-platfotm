54c09ecb1072d3471ed14caae9b3180f6ddf13f6
antoniomartinalbacetepresa-web <antoniomartinalbacetepresa@gmail.com>
2026-09-23 02:56:20 +0200

    feat(fundamentals): activate schema v8

--- backend/app/database/athena_database.py
+++ backend/app/database/athena_database.py
@@ -1,3 +1,3 @@
-from app.database.athena_database_v7 import AthenaConnection, AthenaDatabase
+from app.database.athena_database_v8 import AthenaConnection, AthenaDatabase
 
 __all__ = ["AthenaConnection", "AthenaDatabase"]
